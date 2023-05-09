859ded48b223c7f4032d5f24e737b411fc134000
Emprario <pierre.cameleri@free.fr>
2023-05-09 18:08:31 +0200

    make doc for player and RESSOURCE

--- RESSOURCE.py
+++ RESSOURCE.py
@@ -1,6 +1,6 @@
-REEL_SIZE = 45  # 96px = 1 case
-MAP_SEL = '01.map'
-DEFAULT_ACTION_COUNT = 1
+REEL_SIZE: int = 45  # 96px = 1 case
+MAP_SEL: str = '01.map'
+DEFAULT_ACTION_COUNT: int = 1
 
 if __name__=="__main__":
     print("Please don't run this file ! \n" \
--- player.py
+++ player.py
@@ -2,15 +2,19 @@ from RESSOURCE import DEFAULT_ACTION_COUNT
 from entities import Piece
 
 
+Color = "w" | "b"
+
+
 class Player:
+    """Classe stockant les propriétés d'un joueur"""
 
     def __init__(self, color: str):
-        self.gold = 100
-        self.color = color
+        self.gold: int = 100
+        self.color: Color = color
         self.pieces: list[Piece] = []  # 1 grosse de chaque + 8 pions
         # Not on a case with the "Hide" attribute
         self.visible_pieces: list[Piece] = []
-        self.pointing = False
+        self.pointing: bool = False
         self.possibilite_mvto: list[tuple[int, int]] = []
         self.possibilite_attack: list[tuple[int, int]] = []
         self.sel_piece: Piece = None
