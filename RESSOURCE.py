72224ef8bf0c66abb5e27e6fc24d26cc4eec66c7
Emprario <pierre.cameleri@free.fr>
2023-05-09 17:50:27 +0200

    add .pyi info file and APIDOC folder

--- RESSOURCE.py
+++ RESSOURCE.py
@@ -1,3 +1,9 @@
 REEL_SIZE = 45  # 96px = 1 case
 MAP_SEL = '01.map'
 DEFAULT_ACTION_COUNT = 1
+
+if __name__=="__main__":
+    print("Please don't run this file ! \n" \
+          "Run this instead : \n " \
+          "* (windows) `py main.py` \n " \
+          "* (linux) `chmod +x main.py` then `./main.py` ")
